2d828c63857d563ce616a84c6a24ada2c4a1dd1c
Simon Halimi <halimisimon@gmail.com>
2022-10-10 11:16:38 +0200

    [FIX] User embedding username

--- src/main/java/com/soat/anti_gaspi/domain/User.java
+++ src/main/java/com/soat/anti_gaspi/domain/User.java
@@ -1,15 +1,17 @@
 package com.soat.anti_gaspi.domain;
 
 
-
 class User {
 
+    private final String username;
     private final Email email;
     private final Company company;
 
-    private User(final Email email, Company company) {
+
+    private User(final Email email, Company company, String username) {
         this.email = email;
         this.company = company;
+        this.username = username;
     }
 
     public Email getEmail() {
@@ -27,6 +29,7 @@ class User {
     public static class UserBuilder {
         private Email email;
         private Company company;
+        private String username;
 
         private UserBuilder() {
         }
@@ -41,8 +44,14 @@ class User {
             return this;
         }
 
-        public User build() {
-            return new User(email, company);
+        public UserBuilder username(String username) {
+            this.username = username;
+            return this;
         }
+
+        public User build() {
+            return new User(email, company, username);
+        }
+
     }
 }
