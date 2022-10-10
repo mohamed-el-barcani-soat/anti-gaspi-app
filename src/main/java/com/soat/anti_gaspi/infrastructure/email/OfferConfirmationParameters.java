450464d160141913c18ec31d5d79fe831b4478ce
mohamed-el-barcani-soat <mohamed.el-barcani@soat.fr>
2022-10-10 13:20:21 +0200

    improvement : convert OfferConfirmantionParameters class to record

--- src/main/java/com/soat/anti_gaspi/infrastructure/email/OfferConfirmationParameters.java
+++ src/main/java/com/soat/anti_gaspi/infrastructure/email/OfferConfirmationParameters.java
@@ -1,32 +1,9 @@
 package com.soat.anti_gaspi.infrastructure.email;
 
-public class OfferConfirmationParameters {
-    private final String title;
-    private final String description;
-    private final String validateLink;
-    private final String rejectLink;
-
-    public OfferConfirmationParameters(String title, String description, String validateLink, String rejectLink) {
-        this.title = title;
-        this.description = description;
-        this.validateLink = validateLink;
-        this.rejectLink = rejectLink;
-    }
-
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public String getValidateLink() {
-        return validateLink;
-    }
-
-    public String getRejectLink() {
-        return rejectLink;
-    }
+public record OfferConfirmationParameters(
+        String title,
+        String description,
+        String validateLink,
+        String rejectLink) {
 }
+
