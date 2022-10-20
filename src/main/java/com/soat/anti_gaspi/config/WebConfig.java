68137bd34e616efc2711492a31a9544a8cfd7043
MISHIISoat <masataka.ishii@soat.fr>
2022-10-20 17:18:28 +0200

    refacto(config): remove useless paths

--- src/main/java/com/soat/anti_gaspi/config/WebConfig.java
+++ src/main/java/com/soat/anti_gaspi/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.soat.anti_gaspi.config;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -9,14 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig {
 
-    @Value("${CUSTOMCONNSTR_REACT_ENV}")
-    private String REACT_APP;
-
-    @Value("${CUSTOMCONNSTR_ANGULAR_ENV}")
-    private String ANGULAR_APP;
-
     @Bean
-    public WebMvcConfigurer corsConfigurer() { // TODO : find better way to allow origins than WebMvcConfigurer
+    public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
