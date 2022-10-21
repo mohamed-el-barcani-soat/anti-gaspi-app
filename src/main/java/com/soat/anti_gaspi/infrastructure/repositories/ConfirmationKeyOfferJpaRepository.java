f2d992ba60ea7473c8a956bb51ba02da2a183726
Simon Halimi <halimisimon@gmail.com>
2022-10-21 10:04:36 +0200

    oups mais ca fonctionne promis

--- src/main/java/com/soat/anti_gaspi/infrastructure/repositories/ConfirmationKeyOfferJpaRepository.java
+++ src/main/java/com/soat/anti_gaspi/infrastructure/repositories/ConfirmationKeyOfferJpaRepository.java
@@ -9,5 +9,5 @@ import java.util.Optional;
 @Repository
 public interface ConfirmationKeyOfferJpaRepository extends JpaRepository<ConfirmationKeyOfferEntity, Long> {
     Optional<ConfirmationKeyOfferEntity> findByOfferId(String offerId);
-    Optional<ConfirmationKeyOfferEntity> findById(String offerId);
+    Optional<ConfirmationKeyOfferEntity> findByKey_hash(String keyHash);
 }
--- src/main/java/com/soat/anti_gaspi/infrastructure/repositories/OfferAdapter.java
+++ src/main/java/com/soat/anti_gaspi/infrastructure/repositories/OfferAdapter.java
@@ -53,7 +53,7 @@ public class OfferAdapter implements OfferRepository, FindOfferRepository, FindP
 
     @Override
     public Optional<Offer> findOfferByHash(String hash) {
-        var hashEntity = keyRepository.findById(hash);
+        var hashEntity = keyRepository.findByKey_hash(hash);
 
         if (hashEntity.isEmpty()) return Optional.empty();
         return jpaRepository
